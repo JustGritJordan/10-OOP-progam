901cfb6df3137a5e2298372d791fcdad32fbc45c
JGJ <jordan.gb0@gmail.com>
2023-02-16 00:24:36 -0500

    added npi inquierer, have prompts but not in order

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const inquirer = require("inquirer");
-const fetch = require("node-fetch");
 
-inquirer.prompt(
-  {
-    type: "input",
-    name: "text",
-    message: "Please enter 3 characters:",
-  },
-  {
-    type: "input",
-    name: "text-color",
-    message: "Please enter a color or hexadecimal numer for the logo's colour:",
-  },
-  {
-    type: "input",
-    name: "shape-style",
-    message: "Please enter one of the three, Triangle, Circle, or Square:",
-  },
-  {
-    type: "input",
-    name: "shape-color",
-    message:
-      "Please enter another color or hexadecimal numer for the shape's colour",
-  }
-);
+inquirer.prompt({
+  type: "input",
+  name: "text",
+  message: "Please enter 3 characters:",
+});
+
+inquirer.prompt({
+  type: "input",
+  name: "text-color",
+  message: "Please enter a color or hexadecimal numer for the logo's colour:",
+});
+
+inquirer.prompt({
+  type: "input",
+  name: "shape-style",
+  message: "Please enter one of the three, Triangle, Circle, or Square:",
+});
+
+inquirer.prompt({
+  type: "input",
+  name: "shape-color",
+  message:
+    "Please enter another color or hexadecimal numer for the shape's colour",
+});
