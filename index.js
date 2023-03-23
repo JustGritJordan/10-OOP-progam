158bc695b45be2544e90c64761a719b144e92e3e
JGJ <jordan.gb0@gmail.com>
2023-03-23 11:13:17 -0400

    added .gitignore

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 const inquirer = require("inquirer");
+const {Triangle} = require("./lib/shape")
+const fs = require("fs")
 
-inquirer.prompt({
+inquirer.prompt([{
   type: "input",
   name: "text",
   message: "Please enter 3 characters:",
-});
-
-inquirer.prompt({
-  type: "input",
-  name: "text-color",
-  message: "Please enter a color or hexadecimal numer for the logo's colour:",
-});
-
-inquirer.prompt({
-  type: "input",
-  name: "shape-style",
+},
+{type: "input",
+name: "textColor",
+message: "Please enter a color or hexadecimal numer for the logo's colour:",
+},
+{
+  type: "list",
+  name: "shape",
   message: "Please enter one of the three, Triangle, Circle, or Square:",
-});
-
-inquirer.prompt({
+  choices: ["Triangle", "Circle", "Square"]
+},
+{
   type: "input",
-  name: "shape-color",
+  name: "shapeColor",
   message:
     "Please enter another color or hexadecimal numer for the shape's colour",
-});
+}
+
+]).then(answers => {
+  switch (answers.shape) {
+    case "Triangle":
+      return new Triangle(answers.shapeColor)
+    default:
+      break;
+  }
+}) 
+
+
+
+
+
+
