5ba91f0c787d430e9d90d74f25975115d1cfe2cf
JGJ <jordan.gb0@gmail.com>
2023-04-22 15:09:06 -0400

    final code

--- index.js
+++ index.js
@@ -1,40 +1,65 @@
 const inquirer = require("inquirer");
-const {Triangle} = require("./lib/shape")
-const fs = require("fs")
-
-inquirer.prompt([{
-  type: "input",
-  name: "text",
-  message: "Please enter 3 characters:",
-},
-{type: "input",
-name: "textColor",
-message: "Please enter a color or hexadecimal numer for the logo's colour:",
-},
-{
-  type: "list",
-  name: "shape",
-  message: "Please enter one of the three, Triangle, Circle, or Square:",
-  choices: ["Triangle", "Circle", "Square"]
-},
-{
-  type: "input",
-  name: "shapeColor",
-  message:
-    "Please enter another color or hexadecimal numer for the shape's colour",
-}
-
-]).then(answers => {
-  switch (answers.shape) {
-    case "Triangle":
-      return new Triangle(answers.shapeColor)
-    default:
-      break;
-  }
-}) 
+const { Circle, Square, Triangle } = require("./lib/shape");
+const SVG = require("./lib/svg");
+const { writeFile } = require("fs/promises")
 
 
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "text",
+      message: "Please enter 3 characters:",
+    },
+    {
+      type: "input",
+      name: "textColor",
+      message:
+        "Please enter a color or hexadecimal numer for the logo's text color:",
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "Please enter one of the three, Triangle, Circle, or Square:",
+      choices: ["Triangle", "Circle", "Square"],
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message:
+        "Please enter another color or hexadecimal number for the shape's colour:",
+    },
+  ])
+
+  .then(({ text, textColor, shapeType, shapeColor }) => {
+    let shape;
+    switch (shapeType) {
+      case "Circle":
+        shape = new Circle();
+        break;
+
+      case "Square":
+        shape = new Square();
+        break;
+
+      default:
+        shape = new Triangle();
+        break;
+    }
+    shape.setColor(shapeColor);
+
+    const svg = new SVG();
+    svg.setText(text, textColor);
+    svg.setShape(shape);
+    return writeFile("logo.svg", svg.render());
+  })
+  .then(() => {
+    console.log("Generated logo.svg");
+  })
+  .catch((error) => {
+    console.log(error);
+    console.log("Oops! Something went wrong.");
+  });
 
 
-
-
+// Need to create a function using inputs
